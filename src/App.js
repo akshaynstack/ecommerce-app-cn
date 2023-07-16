aedf73372840b739486bb235bc520a93a6d89c86
AKSHAY N <94807520+akshaynstack@users.noreply.github.com>
2023-07-17 00:46:35 +0530

    init-store

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Products from './components/Products';
-import Footer from './components/Footer';
+import Nav from "./Component/Nav";
+import ProductDetail from "./Component/ProductDetail";
+import AddProduct from "./Component/AddProduct";
+import CartItems from "./Component/CartItems";
+import ProductItemList from "./Component/ProductItemList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { addproducts } from "./actions/index";
+import customFetch from "./apiCall";
+import { useEffect } from "react";
 
 function App() {
+  let productDetailItem = useSelector((state) => state.itemToDisplay);
+
+  const url = "https://my-json-server.typicode.com/akshaynstack/dummy-ecommerce/db";
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let response = customFetch(url, {
+      method: "GET",
+    });
+    response.then((data) => {
+      let modifiedData = data.products.map((item) => {
+        item.edit = true;
+        return item;
+      });
+      window.localStorage.setItem("products", JSON.stringify(modifiedData));
+      let products = JSON.parse(window.localStorage.getItem("products"));
+      dispatch(addproducts(products));
+    });
+  }, []);
+
   return (
     <div className="App">
-      <Navbar />
-      <Products />
-      <Footer />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<ProductItemList />} />
+          <Route path="/addproducts" element={<AddProduct />} />
+          <Route
+            path={`/productdetails/${productDetailItem.id}`}
+            element={<ProductDetail item={productDetailItem} />}
+          />
+          <Route path="/cart" element={<CartItems />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/AddProductButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AddProductButton = ({ handleAdd }) => {
-  return (
-    <button onClick={handleAdd}>Add Product</button>
-  );
-};
-
-export default AddProductButton;
